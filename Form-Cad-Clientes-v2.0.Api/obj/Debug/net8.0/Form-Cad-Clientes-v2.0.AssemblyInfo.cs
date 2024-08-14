d0891a58312212c5488bab9de4e16dc0f1473e55
Rafael Colares <rafaelcolares.dev@gmail.com>
2024-08-14 10:40:44 -0300

    Api finalizada, mas pendente de refatoração e melhora da semantica.

--- Form-Cad-Clientes-v2.0.Api/obj/Debug/net8.0/Form-Cad-Clientes-v2.0.AssemblyInfo.cs
+++ Form-Cad-Clientes-v2.0.Api/obj/Debug/net8.0/Form-Cad-Clientes-v2.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Form-Cad-Clientes-v2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2163131b5486b12622ab4f8bbf207600c56e1a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+853a6965449329cc593ae0979b304ee3cea71aa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Form-Cad-Clientes-v2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Form-Cad-Clientes-v2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
